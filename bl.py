bf4e664410b85a83446a425d4e2ad951ec144118
mema777 <36521000+mema777@users.noreply.github.com>
2024-09-15 18:50:03 +0200

    Update bl.py

--- bl.py
+++ bl.py
@@ -1,17 +1,27 @@
 import pyautogui
 import time
 import random
-import keyboard  # Modul pro detekci klávesových stisků
+import threading
 
-while True:
-    # Pokud je stisknuta klávesa 'q', skript se ukončí
-    if keyboard.is_pressed('q'):
-        print("Program ukončen uživatelem.")
-        break
-    
+# Proměnná pro ukončení skriptu
+running = True
+
+# Funkce pro sledování vstupu uživatele
+def listen_for_exit():
+    global running
+    input("Stiskni Enter pro ukončení programu...\n")
+    running = False
+
+# Vytvoření a spuštění vlákna pro sledování ukončení
+exit_thread = threading.Thread(target=listen_for_exit)
+exit_thread.start()
+
+while running:
     # Simulace stisknutí klávesy F5
     pyautogui.press('f5')
-    
+
     # Čekání náhodný počet sekund mezi 5.0 a 9.0
     wait_time = random.uniform(5.0, 9.0)
     time.sleep(wait_time)
+
+print("Program ukončen.")
